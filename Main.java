0ef2442156f616225f66e6be98140882d4e7bbea
Angkit Khadka <khadkaankit85@gmail.com>
2024-01-18 14:25:06 +0000

    halfway

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 
 class Main {
+  static int userChoice;
+
   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    
+
     System.out.println("A1A Carwash");
     // Using Escape sequence character to get a good formatting of output:)
     System.out.println("Type Of Car\t\t\t Price");
@@ -8,10 +14,17 @@ class Main {
     System.out.println("3. Sports Car\t\t $40,000\n4. Hatchback\t\t $18,000");
     System.out.println("5. Convertible\t\t $35,000\n6. Luxury Car\t\t $50,000");
     System.out.println("7. Quit");
+  System.out.println("Enter Below( 1 to 7): ");
+  try{
+  userChoice=sc.nextInt();
+  InputValidator(userChoice);
+  }
+  catch(exception e){
+    System.out.println("Enter a String Please");
+  }
+  }
+  static void InputValidator(int a){
+    
   }
 
-  // @Test
-  // void addition() {
-  // assertEquals(2, 1 + 1);
-  // }
 }
