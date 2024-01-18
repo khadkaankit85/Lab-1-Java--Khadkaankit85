5c9885332d0000f24a453ac6402035991ce6f617
Angkit Khadka <khadkaankit85@gmail.com>
2024-01-18 14:54:21 +0000

    new branch created to change code

--- Main.java
+++ Main.java
@@ -5,31 +5,32 @@ class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    do {
+      System.out.println("A1A Carwash");
+      System.out.println("Type Of Car\t\t\t Price");
+      System.out.println("1. Sedan\t\t\t $20,000\n2. SUV\t\t\t\t $30,000");
+      System.out.println("3. Sports Car\t\t $40,000\n4. Hatchback\t\t $18,000");
+      System.out.println("5. Convertible\t\t $35,000\n6. Luxury Car\t\t $50,000");
+      System.out.println("7. Quit");
+      System.out.println("Enter Below (1 to 7): ");
+      try {
+        userChoice = sc.nextInt();
+        InputValidator(userChoice);
+      } catch (IllegalArgumentException e) {
+        System.out.println("HINT: Enter a Number between 1 to 7");
+        System.out.println();
+      }
 
-    System.out.println("A1A Carwash");
-    // Using Escape sequence character to get a good formatting of output:)
-    System.out.println("Type Of Car\t\t\t Price");
-    System.out.println("1. Sedan\t\t\t $20,000\n2. SUV\t\t\t\t $30,000");
-    System.out.println("3. Sports Car\t\t $40,000\n4. Hatchback\t\t $18,000");
-    System.out.println("5. Convertible\t\t $35,000\n6. Luxury Car\t\t $50,000");
-    System.out.println("7. Quit");
-    System.out.println("Enter Below( 1 to 7): ");
-    try {
-      userChoice = sc.nextInt();
-      InputValidator(userChoice);
-    } catch (exception e) {
-      System.out.println("Unexpected Input Received.");
-    } catch (IllegalArgumentException e) {
-      System.out.println("HINT: Enter a Number between One to 7");
-      System.out.println();
-    }
+    } while (InputValidator(userChoice));
+
+    sc.close();
   }
 
-  static void InputValidator(int a) {
-    if (a < 0 && a > 7) {
-      throw new IllegalArgumentExcenption("Error: Invalid Input.");
-
+  static boolean InputValidator(int a) {
+    if (a < 1 || a > 7) {
+      throw new IllegalArgumentException("Error: Invalid Input.");
+    } else {
+      return a != 7;
     }
   }
-
 }
