ef991aedbe9cc0c63686b5ece1fd93e232bd3ea5
Angkit Khadka <khadkaankit85@gmail.com>
2024-01-18 15:11:27 +0000

    user input getting correctly

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    do {
+
+    while (true) {
       System.out.println("A1A Carwash");
       System.out.println("Type Of Car\t\t\t Price");
       System.out.println("1. Sedan\t\t\t $20,000\n2. SUV\t\t\t\t $30,000");
@@ -13,24 +14,34 @@ class Main {
       System.out.println("5. Convertible\t\t $35,000\n6. Luxury Car\t\t $50,000");
       System.out.println("7. Quit");
       System.out.println("Enter Below (1 to 7): ");
+
       try {
         userChoice = sc.nextInt();
-        InputValidator(userChoice);
-      } catch (IllegalArgumentException e) {
-        System.out.println("HINT: Enter a Number between 1 to 7");
-        System.out.println();
+        if (InputValidator(userChoice)) {
+          System.out.println("Error: Invalid input. Enter a number between 1 and 7.");
+          continue;
+        }
+      } catch (Exception e) {
+        System.out.println("Error: Unexpected input. Please enter a number.");
+        // Clear the buffer to prevent an infinite loop
+        sc.nextLine();
+        continue;
       }
 
-    } while (InputValidator(userChoice));
+      if (userChoice == 7) {
+        break; // Exit the loop if the user chooses to quit
+      }
+      else {
+        ;
+        if (userChoice==1){
+        System.out.println();
+      }
+    }
 
     sc.close();
   }
 
   static boolean InputValidator(int a) {
-    if (a < 1 || a > 7) {
-      throw new IllegalArgumentException("Error: Invalid Input.");
-    } else {
-      return a != 7;
-    }
+    return a < 1 || a > 7;
   }
 }
